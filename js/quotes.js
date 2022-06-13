597d2e93138f1fdba4d8b10b217d4405229bcd82
hy-HA <73115727+hy-HA@users.noreply.github.com>
2022-06-13 21:08:17 +0900

    img height

--- js/quotes.js
+++ js/quotes.js
@@ -68,7 +68,7 @@ const todaysQuote = quotes[Math.floor(Math.random()*quotes.length)];
 
 
 const images = [
-    "111.png","222.png","333.png","444.png","555.png","666.png","777.png","888.png","999.png",
+    "2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png",
 ]
 const chosenImg = images[Math.floor(Math.random()*images.length)];
 
